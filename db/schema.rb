d6c8d7779aea7bd6bf514ddd61cc5aaec66cbd19
Pat <Patgiok@gmail.com>
2022-03-09 13:49:02 -0800

    cars scaffold migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_09_203103) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_214812) do
+  create_table "cars", force: :cascade do |t|
+    t.integer "year"
+    t.string "make"
+    t.string "model"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "people", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
