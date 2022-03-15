5b2d9cd1d1e31d4ed1bed7602014387a4994be6c
Pat <Patgiok@gmail.com>
2022-03-14 23:50:07 -0700

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-People.create![
-    {firstname: 'Josh', lastname: 'Brolin', email: 'jb@gmail.com'},
-    {firstname: 'Emily', lastname: 'Blunt', email: 'EmilyB@gmail.com'},
-    {firstname: 'Benicio', lastname: 'Del-Toro', email: 'Sicario@gmail.com'}
-]
+People = [ {firstname: 'Josh', lastname: 'Brolin', email: 'jb@gmail.com'}, {firstname: 'Emily', lastname: 'Blunt', email: 'EmilyB@gmail.com'}, {firstname: 'Benicio', lastname: 'Del-Toro', email: 'Sicario@gmail.com'} ]
+People.each do |p| 
+  Person.create! p 
+end
 
-Cars.create![
-    {year: '2019', make: 'Audi', model: 'R8', price: '167000', person_id: '1'},
-    {year: '2010', make: 'Toyota', model: 'Camry', price: '6500', person_id: '2'},
-    {year: '2009', make: 'Honda', model: 'Civic', price: '4500', person_id: '3'},
-    {year: '2017', make: 'BMW', model: 'X5', price: '15000', person_id: '2'}
-]
+Cars = [     {year: '2019', make: 'Audi', model: 'R8', price: '167000', person_id: '1'}, {year: '2010', make: 'Toyota', model: 'Camry', price: '6500', person_id: '2'}, {year: '2009', make: 'Honda', model: 'Civic', price: '4500', person_id: '3'}, {year: '2017', make: 'BMW', model: 'X5', price: '15000', person_id: '2'} ]
+Cars.each do |p|
+    Car.create! p
+end
